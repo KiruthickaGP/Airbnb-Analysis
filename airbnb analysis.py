a98c7610c6a9ba8be760a39f146c7473c0234dbc
Kiruthicka GP <130207892+KiruthickaGP@users.noreply.github.com>
2023-10-25 16:44:59 +0530

    Airbnb analysis

---
+++ airbnb analysis.py
@@ -0,0 +1,182 @@
+import select
+import selectors
+import streamlit as st
+from streamlit_option_menu import option_menu
+import plotly.express as px
+import pandas as pd
+import os
+from PIL import Image
+import warnings
+import pymongo
+import mysql.connector as sql
+warnings.filterwarnings('ignore')
+
+#PART 2
+# Open the image file for the page icon
+icon = Image.open(r"E:\Guvidatascience\Projects\Airbnb_analysis\Airbnb.png")
+
+# Set page configurations with background color
+st.set_page_config(
+    page_title="Airbnb_analysis | By Kiruthicka",
+    page_icon=icon,
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={'About': """# This app is created by *Kiruthicka!*"""}
+)
+
+# Add background color using CSS
+background_color = """
+<style>
+    body {
+        background-color: #ff545b;  /* Set background color to #F7EBED*/            #AntiqueWhite color
+    }
+    .stApp {
+        background-color: #F7EBED; /* Set background-color for the entire app */
+    }
+</style>
+"""
+#AntiqueWhite color #F7EBED
+st.markdown(background_color, unsafe_allow_html=True)
+
+st.snow()
+
+# CREATING OPTION MENU
+with st.sidebar:
+    selected = option_menu(
+        None, ["Home", "Extract and Transform"],
+        icons=["house-door-fill", "tools"],
+        default_index=0,
+        orientation="Horizontal",
+        styles={
+            "nav-link": {
+                "font-size": "30px",
+                "font-family": "Fira Sans",
+                "font-weight": "Bold",
+                "text-align": "center",
+                "margin": "30px",
+                "--hover-color": "#ff545b"#Grayish red
+            },
+            "icon": {"font-size": "30px"},
+            "container": {"max-width": "6000px"},
+            "nav-link-selected": {
+                "background-color": "#ff545b",#Grey Pink
+                "color": "Grey Pink",
+            }
+        }
+    )
+
+
+
+
+# HOME PAGE
+if selected == "Home":
+    # Title Image
+    col1, col2 = st.columns([3, 1])  # Adjust column widths as needed
+    col1.markdown("<h2 class='black-text'>Domain : Travel Industry, Property Management and Tourism </h2>", unsafe_allow_html=True)
+    col1.markdown("<h2 class='black-text'>Technologies used : Python Scripting, Data Preprocessing, Visualization, EDA, Streamlit, MongoDb,  Tableau</h2>", unsafe_allow_html=True)
+    col1.markdown("<h2 class='black-text'>Overview : Airbnb is an American San Francisco-based company operating an online marketplace for short- and long-term homestays and experiences. The company acts as a broker and charges a commission from each booking. The company was founded in 2008 by Brian Chesky, Nathan Blecharczyk, and Joe Gebbia. Airbnb is a shortened version of its original name, AirBedandBreakfast.com. The company is credited with revolutionizing the tourism industry, while also having been the subject of intense criticism by residents of tourism hotspot cities like Barcelona and Venice for enabling an unaffordable increase in home rents, and for a lack of regulation</h2>", unsafe_allow_html=True)
+    col2.image(icon,  use_column_width=True)
+
+
+# Extract and Transform
+if selected == "Extract and Transform":
+ fl = st.file_uploader(":file_folder: Upload a file", type=(["csv", "txt", "xlsx", "xls"]))
+ if fl is not None:
+    filename = fl.name
+    st.write(filename)
+    df = pd.read_csv(filename, encoding="ISO-8859-1")
+ else:
+    os.chdir(r"E:\Guvidatascience\Projects\Airbnb_analysis")
+    df = pd.read_csv("airbnb csv file.csv", encoding="ISO-8859-1")
+
+ # Create for neighbourhood_group
+ neighbourhood_group = st.sidebar.multiselect("Pick your neighbourhood_group", df["neighbourhood_group"].unique())
+ if not neighbourhood_group:
+     df2 = df.copy()
+ else:
+     df2 = df[df["neighbourhood_group"].isin(neighbourhood_group)]
+
+ # Create for neighbourhood
+ neighbourhood = st.sidebar.multiselect("Pick the neighbourhood", df2["neighbourhood"].unique())
+ if not neighbourhood:
+     df3 = df2.copy()
+ else:
+     df3 = df2[df2["neighbourhood"].isin(neighbourhood)]
+
+ # Filter the data based on neighbourhood_group, neighbourhood
+
+ if not neighbourhood_group and not neighbourhood:
+     filtered_df = df
+ elif not neighbourhood:
+     filtered_df = df[df["neighbourhood_group"].isin(neighbourhood_group)]
+ elif not neighbourhood_group:
+     filtered_df = df[df["neighbourhood"].isin(neighbourhood)]
+ elif neighbourhood:
+     filtered_df = df3[df["neighbourhood"].isin(neighbourhood)]
+ elif neighbourhood_group:
+     filtered_df = df3[df["neighbourhood_group"].isin(neighbourhood_group)]
+ elif neighbourhood_group and neighbourhood:
+     filtered_df = df3[df["neighbourhood_group"].isin(neighbourhood_group) & df3["neighbourhood"].isin(neighbourhood)]
+ else:
+     filtered_df = df3[df3["neighbourhood_group"].isin(neighbourhood_group) & df3["neighbourhood"].isin(neighbourhood)]
+
+ room_type_df = filtered_df.groupby(by=["room_type"], as_index=False)["price"].sum()
+
+ col1, col2 = st.columns(2)
+ with col1:
+    st.subheader("room_type_ViewData")
+    fig = px.bar(room_type_df, x="room_type", y="price", text=['${:,.2f}'.format(x) for x in room_type_df["price"]],
+                 template="seaborn")
+    st.plotly_chart(fig, use_container_width=True, height=200)
+
+ with col2:
+    st.subheader("neighbourhood_group_ViewData")
+    fig = px.pie(filtered_df, values="price", names="neighbourhood_group", hole=0.5)
+    fig.update_traces(text=filtered_df["neighbourhood_group"], textposition="outside")
+    st.plotly_chart(fig, use_container_width=True)
+
+ cl1, cl2 = st.columns((2))
+ with cl1:
+    with st.expander("room_type wise price"):
+        st.write(room_type_df.style.background_gradient(cmap="Blues"))
+        csv = room_type_df.to_csv(index=False).encode('utf-8')
+        st.download_button("Download Data", data=csv, file_name="room_type.csv", mime="text/csv",
+                           help='Click here to download the data as a CSV file')
+
+ with cl2:
+    with st.expander("neighbourhood_group wise price"):
+        neighbourhood_group = filtered_df.groupby(by="neighbourhood_group", as_index=False)["price"].sum()
+        st.write(neighbourhood_group.style.background_gradient(cmap="Oranges"))
+        csv = neighbourhood_group.to_csv(index=False).encode('utf-8')
+        st.download_button("Download Data", data=csv, file_name="neighbourhood_group.csv", mime="text/csv",
+                           help='Click here to download the data as a CSV file')
+
+ # Create a scatter plot
+ data1 = px.scatter(filtered_df, x="neighbourhood_group", y="neighbourhood", color="room_type")
+ data1['layout'].update(title="Room_type in the Neighbourhood and Neighbourhood_Group wise data using Scatter Plot.",
+                        titlefont=dict(size=20), xaxis=dict(title="Neighbourhood_Group", titlefont=dict(size=20)),
+                        yaxis=dict(title="Neighbourhood", titlefont=dict(size=20)))
+ st.plotly_chart(data1, use_container_width=True)
+
+ with st.expander("Detailed Room Availability and Price View Data in the Neighbourhood"):
+     st.write(filtered_df.iloc[:500, 1:20:2].style.background_gradient(cmap="Oranges"))
+
+ # Download orginal DataSet
+ csv = df.to_csv(index=False).encode('utf-8')
+ st.download_button('Download Data', data=csv, file_name="Data.csv", mime="text/csv")
+
+ import plotly.figure_factory as ff
+
+ st.subheader(":point_right: Neighbourhood_group wise Room_type and Minimum stay nights")
+ with st.expander("Summary_Table"):
+    df_sample = df[0:5][["neighbourhood_group", "neighbourhood", "reviews_per_month", "room_type", "price", "minimum_nights", "host_name"]]
+    fig = ff.create_table(df_sample, colorscale="Cividis")
+    st.plotly_chart(fig, use_container_width=True)
+
+ # map function for room_type
+
+# If your DataFrame has columns 'Latitude' and 'Longitude':
+ st.subheader("Airbnb Analysis in Map view")
+ df = df.rename(columns={"Latitude": "lat", "Longitude": "lon"})
+
+ st.map(df)
